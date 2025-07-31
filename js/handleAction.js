69e055dff1cabd9905ec4c68636a49d7017ddc64
Wensheng Deng <wd35@nyu.edu>
2025-07-31 13:19:36 -0400

    Disable the button for downloading file list in CSV

--- js/handleAction.js
+++ js/handleAction.js
@@ -9,6 +9,22 @@ ExternalModules.NYU.MaskIPFileRepoModule.ajax("loaded", {})
 		$('#file-repository-table').on('init.dt', function () {
 		    table.innerHTML = table.innerHTML.replace(ipv4Regex, "***.***.***.***");
 		});
+
+		let tableParent = document.getElementById("file-repository-table-parent");
+		const callback = function(mutationsList, observer) {
+		    for (let mutation of mutationsList) {
+			if (mutation.target.className == "ItemListBreadcrumbs") {
+			    let dwnlButton = mutation.target.getElementsByClassName("btn btn-xs btn-defaultrc text-successrc")[0]; 
+			    if (dwnlButton.innerText == " Download file list") {
+				dwnlButton.disabled = true;
+				break;
+			    }
+			}
+		    }
+		};
+		const observer = new MutationObserver(callback);
+		const config = { childList: true, subtree: true };
+		observer.observe(tableParent, config);
 	    } else {
 		console.error("Error in response!");
 	    }
